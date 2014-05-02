b530ef7ce7ef5b4ef9f3518203f6e4982825a678
Kinsey Ann <kbooboo@kinseys-MacBook-Pro-2.local>
2014-05-02 10:26:06 -0600

    Setting up SendGrid on Heroku

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -1,6 +1,27 @@
+require 'mail'
+
 class Notifier < ActionMailer::Base
   def ask_rach_email(ask)
     @ask = ask
     mail(to: "rach.durham@gmail.com", from: ask.email, subject: ask.subject)
+  end
+
+  Mail.defaults do
+    delivery_method :smtp, {
+      :address => 'smtp.sendgrid.net',
+      :port => '587',
+      :domain => 'heroku.com',
+      :user_name => ENV['SENDGRID_USERNAME'],
+      :password => ENV['SENDGRID_PASSWORD'],
+      :authentication => :plain,
+      :enable_starttls_auto => true
+    }
+  end
+
+  Mail.deliver do
+    to 'kinseyann505@gmail.com'
+    from 'sender@example.com'
+    subject 'testing send mail'
+    body 'Sending email with Ruby through SendGrid!'
   end
 end
